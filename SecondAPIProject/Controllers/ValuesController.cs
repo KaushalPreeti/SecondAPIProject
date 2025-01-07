2f4cac2c25c37c396e5cfcf8f120d8cf1bc6f72a
KaushalPreeti <154746427+KaushalPreeti@users.noreply.github.com>
2025-01-07 16:03:16 +0530

    Add project files.

---
+++ SecondAPIProject/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecondAPIProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("TestName")]
+        public string getData()
+        {
+            return "Hi Kaushal";
+        }
+
+    }
+}
